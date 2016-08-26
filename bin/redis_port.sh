ca79f7899b0c4bf5edf08f74ad557597e9eefbaa
chenzhe07 <chenzhe07@gmail.com>
2016-08-26 11:55:46 +0800

    add redis_port.sh to discovery redis port

---
+++ bin/redis_port.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# discovery port for zabbix redis monitor
+
+exec 2>&1
+set -e
+
+netstat -tunlp | grep redis | perl -ne '
+   BEGIN{
+      my $i = 0;
+      my %redis_port;
+   }
+   chomp;
+   $redis_port{$1}++ if /:(\d+)\s/;
+   END {
+      $| = 1;
+      my $data = "{\"data\":[";
+      foreach my $key (keys %redis_port){
+         $i++;
+         $data .= "\{\"{#REDISPORT}\":\"$key\"\}";
+         $data .= $i == keys %redis_port
+               ?  "[}"
+               :  ",";
+      }
+      print $data;
+   }
+'
